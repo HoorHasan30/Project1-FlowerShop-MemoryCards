db7b5958014c8ab1dac2c0c1eae2479d0689f600
HoorHasan30 <202301820@student.polytechnic.bh>
2026-06-25 12:45:13 +0300

    add cards front & back sides

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 /*------------------------ Cached Element References ------------------------*/
+// haeder 
+const timeE = document.querySelector("#time")
+const stateE = document.querySelector("#game-state")
+const resetE = document.querySelector("reset")
 
+// cards
+const cards = document.querySelectorAll(".cards")
 
+const frontE = document.querySelectorAll(".card-front")
+const backE = document.querySelectorAll(".card-back")
+
+// vases
+const daisyE = document.querySelector("#daisy")
+const danE = document.querySelector("#dan")
+const lavenderE = document.querySelector("#lavender")
+const mixE = document.querySelector("#mix")
+const sunflowerE = document.querySelector("#sunflower")
+const tulipE = document.querySelector("#tulip")
 
 
 /*-------------------------------- Constants --------------------------------*/
-
+const imgs = [
+    "./assets/vases/daisy.png",
+    "./assets/vases/daisy.png",
+    "./assets/vases/dan.png",
+    "./assets/vases/dan.png",
+    "./assets/vases/lavender.png",
+    "./assets/vases/lavender.png",
+    "./assets/vases/mix.png",
+    "./assets/vases/mix.png",
+    "./assets/vases/sunflower.png",
+    "./assets/vases/sunflower.png",
+    "./assets/vases/tulip.png",
+    "./assets/vases/tulip.png"
+]
 
 
 
